6040199cc144947e1be74d33f28ac795cbaa12cc
beyondkmp <beyondkmp@gmail.com>
2018-11-27 00:26:58 +0800

    Chore: add route for rules and updateConfigs

--- hub/route/configs.go
+++ hub/route/configs.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/Dreamacro/clash/hub/executor"
 	"github.com/Dreamacro/clash/log"
-	T "github.com/Dreamacro/clash/tunnel"
 	P "github.com/Dreamacro/clash/proxy"
+	T "github.com/Dreamacro/clash/tunnel"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
@@ -15,6 +15,7 @@ import (
 func configRouter() http.Handler {
 	r := chi.NewRouter()
 	r.Get("/", getConfigs)
+	r.Put("/", updateConfigs)
 	return r
 }
 
@@ -32,7 +33,7 @@ func getConfigs(w http.ResponseWriter, r *http.Request) {
 	render.Respond(w, r, general)
 }
 
-func pointerOrDefault (p *int, def int) int {
+func pointerOrDefault(p *int, def int) int {
 	if p != nil {
 		return *p
 	}
@@ -51,7 +52,7 @@ func updateConfigs(w http.ResponseWriter, r *http.Request) {
 	if general.AllowLan != nil {
 		P.SetAllowLan(*general.AllowLan)
 	}
-	
+
 	ports := P.GetPorts()
 	P.ReCreateHTTP(pointerOrDefault(general.Port, ports.Port))
 	P.ReCreateSocks(pointerOrDefault(general.SocksPort, ports.SocksPort))
