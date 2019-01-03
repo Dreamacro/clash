ce68c72e96ee403b48ef05aa96aea7e049905dba
Yonghui Zhang <comzyh@gmail.com>
2019-01-04 04:33:49 +0800

    check whether ProxyGroups forms a DAG, and sort ProxyGroups by their inner dependency relation

--- config/utils.go
+++ config/utils.go
@@ -34,3 +34,72 @@ func or(pointers ...*int) *int {
 	}
 	return pointers[len(pointers)-1]
 }
+
+// check if ProxyGroups form DAG(Directed Acyclic Graph), and sort all ProxyGroups by dependency order
+// meanwhile, record the original index in the config file
+func ProxyGroupsDagSort(groupsConfig []map[string]interface{}) error {
+
+	type Node struct {
+		indegree int
+		topo     int // Topological order
+		data     map[string]interface{}
+	}
+
+	graph := make(map[string]*Node)
+
+	// build dependency graph
+	for idx, mapping := range groupsConfig {
+		mapping["configIdx"] = idx // record original order from configfile
+		groupName, existName := mapping["name"].(string)
+		if !existName {
+			return fmt.Errorf("ProxyGroup %d: missing name", idx)
+		}
+		if node, ok := graph[groupName]; ok {
+			if node.data != nil {
+				return fmt.Errorf("ProxyGroup %s: the duplicate name", groupName)
+			}
+			node.data = mapping
+		} else {
+			graph[groupName] = &Node{0, -1, mapping}
+		}
+		proxies, existProxies := mapping["proxies"]
+		if !existProxies {
+			return fmt.Errorf("ProxyGroup %s: proxies is requried", groupName)
+		}
+		for _, proxy := range proxies.([]interface{}) {
+			proxy := proxy.(string)
+			if node, ex := graph[proxy]; ex {
+				node.indegree += 1
+			} else {
+				graph[proxy] = &Node{1, -1, nil}
+			}
+		}
+	}
+	// Topological Sort
+	index := 0
+	for len(graph) != 0 {
+		loopDetected := true
+		for name, node := range graph {
+			if node.indegree == 0 { // no one depend on it
+				if node.data != nil {
+					index += 1
+					groupsConfig[len(groupsConfig)-index] = node.data
+					for _, proxy := range node.data["proxies"].([]interface{}) {
+						child, _ := graph[proxy.(string)]
+						child.indegree -= 1
+					}
+				}
+				delete(graph, name)
+				loopDetected = false
+				break
+			}
+		}
+		if loopDetected {
+			// TODO: Tell user where the loop occurs
+			// We can locate the loop if we use the reversed graph
+			return fmt.Errorf("Loop detected in ProxyGroup")
+		}
+	}
+
+	return nil
+}
