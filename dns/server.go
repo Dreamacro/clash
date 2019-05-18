e89a7cd4b4c2eef057cf44ae27f23abfdd90cdfb
kr328 <kr328@users.noreply.github.com>
2019-05-18 17:41:01 +0800

    remove some comments

--- dns/server.go
+++ dns/server.go
@@ -73,7 +73,6 @@ func (s *Server) handleFakeIP(r *D.Msg) (msg *D.Msg, err error) {
 		putMsgToCache(s.r.cache, "fakeip:"+q.String(), msg)
 		putMsgToCache(s.r.cache, ip.String(), msg)
 
-		// putMsgToCache depend on msg ttl to set cache expired time, then set msg ref ttl to 1
 		setMsgTTL(msg, 1)
 	}()
 
