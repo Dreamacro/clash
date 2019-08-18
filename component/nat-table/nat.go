e39bc114d3eef752a14927b9bbd3c9c91a079f95
Jason <xjasonlyu@gmail.com>
2019-08-18 17:27:58 +0800

    rewrite nat-table

--- component/nat-table/nat.go
+++ component/nat-table/nat.go
@@ -2,97 +2,65 @@ package nat
 
 import (
 	"net"
-	"runtime"
 	"sync"
-	"time"
 )
 
+// Packet NAT table
 type Table struct {
-	*table
-}
-
-type table struct {
-	mapping sync.Map
-	janitor *janitor
-	timeout time.Duration
+	mu      sync.RWMutex
+	mapping map[string]*element
 }
 
 type element struct {
-	Expired    time.Time
-	RemoteAddr net.Addr
-	RemoteConn net.PacketConn
+	pc   net.PacketConn
+	addr net.Addr
 }
 
-func (t *table) Set(key net.Addr, rConn net.PacketConn, rAddr net.Addr) {
-	// set conn read timeout
-	rConn.SetReadDeadline(time.Now().Add(t.timeout))
-	t.mapping.Store(key, &element{
-		RemoteAddr: rAddr,
-		RemoteConn: rConn,
-		Expired:    time.Now().Add(t.timeout),
-	})
+func NewTable() *Table {
+	return &Table{
+		mapping: make(map[string]*element),
+	}
 }
 
-func (t *table) Get(key net.Addr) (rConn net.PacketConn, rAddr net.Addr) {
-	item, exist := t.mapping.Load(key)
-	if !exist {
+func (t *Table) Get(key string) (pc net.PacketConn, addr net.Addr) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	ele, ok := t.mapping[key]
+	if !ok {
 		return
 	}
-	elm := item.(*element)
-	// expired
-	if time.Since(elm.Expired) > 0 {
-		t.mapping.Delete(key)
-		elm.RemoteConn.Close()
-		return
-	}
-	// reset expired time
-	elm.Expired = time.Now().Add(t.timeout)
-	return elm.RemoteConn, elm.RemoteAddr
+	return ele.pc, ele.addr
 }
 
-func (t *table) cleanup() {
-	t.mapping.Range(func(k, v interface{}) bool {
-		key := k.(net.Addr)
-		elm := v.(*element)
-		if time.Since(elm.Expired) > 0 {
-			t.mapping.Delete(key)
-			elm.RemoteConn.Close()
+func (t *Table) Add(key string, pc net.PacketConn, addr net.Addr, f func()) {
+	t.Set(key, pc, addr)
+
+	go func() {
+		f()
+		if pc, _ := t.Del(key); pc != nil {
+			pc.Close()
 		}
-		return true
-	})
+	}()
 }
 
-type janitor struct {
-	interval time.Duration
-	stop     chan struct{}
-}
+func (t *Table) Set(key string, pc net.PacketConn, addr net.Addr) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
 
-func (j *janitor) process(t *table) {
-	ticker := time.NewTicker(j.interval)
-	for {
-		select {
-		case <-ticker.C:
-			t.cleanup()
-		case <-j.stop:
-			ticker.Stop()
-			return
-		}
+	t.mapping[key] = &element{
+		pc:   pc,
+		addr: addr,
 	}
 }
 
-func stopJanitor(t *Table) {
-	t.janitor.stop <- struct{}{}
-}
+func (t *Table) Del(key string) (pc net.PacketConn, addr net.Addr) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
 
-// New return *Cache
-func New(interval time.Duration) *Table {
-	j := &janitor{
-		interval: interval,
-		stop:     make(chan struct{}),
+	if ele, ok := t.mapping[key]; ok {
+		delete(t.mapping, key)
+		return ele.pc, ele.addr
 	}
-	t := &table{janitor: j, timeout: interval}
-	go j.process(t)
-	T := &Table{t}
-	runtime.SetFinalizer(T, stopJanitor)
-	return T
+	return
 }
