e09ef9f2d46e3efbdcf22b6693cb2b79cc863ac7
yuguorui <yuguorui96@gmail.com>
2019-10-14 17:07:38 +0800

    Feature: Linux IPv6 Redirect support

--- proxy/redir/tcp_linux.go
+++ proxy/redir/tcp_linux.go
@@ -34,18 +34,62 @@ func parserPacket(conn net.Conn) (socks5.Addr, error) {
 	return addr, err
 }
 
+func isIPv6socket(fd uintptr) (bool, error) {
+	peer, err := syscall.Getpeername(int(fd))
+	switch t := peer.(type) {
+	case *syscall.SockaddrInet4:
+		return false, err
+	case *syscall.SockaddrInet6:
+		ip := net.IP(t.Addr[:])
+		if ip.To4() != nil {
+			return false, nil
+		} else {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
 // Call getorigdst() from linux/net/ipv4/netfilter/nf_conntrack_l3proto_ipv4.c
 func getorigdst(fd uintptr) (socks5.Addr, error) {
-	raw := syscall.RawSockaddrInet4{}
-	siz := unsafe.Sizeof(raw)
-	if err := socketcall(GETSOCKOPT, fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&raw)), uintptr(unsafe.Pointer(&siz)), 0); err != nil {
+	isIPv6, _ := isIPv6socket(fd)
+	var level uintptr = syscall.IPPROTO_IP
+	var optname uintptr = SO_ORIGINAL_DST
+	optval := syscall.RawSockaddrAny{}
+	optlen := unsafe.Sizeof(optval)
+
+	if isIPv6 {
+		level = syscall.IPPROTO_IPV6
+		optname = IP6T_SO_ORIGINAL_DST
+	}
+
+	if err := socketcall(GETSOCKOPT, fd, level, optname, uintptr(unsafe.Pointer(&optval)), uintptr(unsafe.Pointer(&optlen)), 0); err != nil {
 		return nil, err
 	}
 
-	addr := make([]byte, 1+net.IPv4len+2)
-	addr[0] = socks5.AtypIPv4
-	copy(addr[1:1+net.IPv4len], raw.Addr[:])
-	port := (*[2]byte)(unsafe.Pointer(&raw.Port)) // big-endian
-	addr[1+net.IPv4len], addr[1+net.IPv4len+1] = port[0], port[1]
-	return addr, nil
+	/*
+	   The SOCKS request is formed as follows:
+	        +----+-----+-------+------+----------+----------+
+	        |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+	        +----+-----+-------+------+----------+----------+
+	        | 1  |  1  | X'00' |  1   | Variable |    2     |
+	        +----+-----+-------+------+----------+----------+
+	*/
+	if !isIPv6 {
+		addr := make([]byte, 1+net.IPv4len+2)
+		addr[0] = socks5.AtypIPv4
+		ipv4Addr := (*syscall.RawSockaddrInet4)(unsafe.Pointer(&optval))
+		copy(addr[1:1+net.IPv4len], ipv4Addr.Addr[:])
+		port := (*[2]byte)(unsafe.Pointer(&ipv4Addr.Port)) // big-endian
+		addr[1+net.IPv4len], addr[1+net.IPv4len+1] = port[0], port[1]
+		return addr, nil
+	} else {
+		addr := make([]byte, 1+net.IPv6len+2)
+		addr[0] = socks5.AtypIPv6
+		ipv6Addr := (*syscall.RawSockaddrInet6)(unsafe.Pointer(&optval))
+		copy(addr[1:1+net.IPv6len], ipv6Addr.Addr[:])
+		port := (*[2]byte)(unsafe.Pointer(&ipv6Addr.Port)) // big-endian
+		addr[1+net.IPv6len], addr[1+net.IPv6len+1] = port[0], port[1]
+		return addr, nil
+	}
 }
