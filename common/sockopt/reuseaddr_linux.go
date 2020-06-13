365762b4742f07f7d8aae5d1d76d786d2d00ea83
limgmk <limgmkgm@gmail.com>
2020-06-13 20:45:54 +0800

    Fix: cannot listen socks5 port on wsl

--- common/sockopt/reuseaddr_linux.go
+++ common/sockopt/reuseaddr_linux.go
@@ -2,6 +2,8 @@ package sockopt
 
 import (
 	"net"
+	"os/exec"
+	"strings"
 	"syscall"
 )
 
@@ -13,7 +15,24 @@ func UDPReuseaddr(c *net.UDPConn) (err error) {
 
 	rc.Control(func(fd uintptr) {
 		err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+		if err != nil && IsWSL() == true {
+			err = nil
+		}
 	})
+
+	return
+}
+
+func IsWSL() (isWSL bool) {
+	cmd := exec.Command("uname", "-r")
+	kernelInfo, err := cmd.CombinedOutput()
+	if err != nil {
+		return false
+	}
+
+	if strings.Contains(string(kernelInfo), "Microsoft") {
+		return true
+	}
 
 	return
 }
