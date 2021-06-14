542e46b5d77e6612ab9ab6b30868be87fab85b8b
kr328 <kr328app@outlook.com>
2021-06-14 13:44:05 +0800

    chore: use spaces instead of tab

--- listener/http/client.go
+++ listener/http/client.go
@@ -1,38 +1,38 @@
 package http
 
 import (
-	"context"
-	"errors"
-	"net"
-	"net/http"
-	"time"
+    "context"
+    "errors"
+    "net"
+    "net/http"
+    "time"
 
-	"github.com/Dreamacro/clash/adapter/inbound"
-	C "github.com/Dreamacro/clash/constant"
+    "github.com/Dreamacro/clash/adapter/inbound"
+    C "github.com/Dreamacro/clash/constant"
 )
 
 const (
-	defaultClientTimeout = time.Minute
+    defaultClientTimeout = time.Minute
 )
 
 func newClient(source net.Addr, in chan<- C.ConnContext) *http.Client {
-	return &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(context context.Context, network, address string) (net.Conn, error) {
-				if network != "tcp" && network != "tcp4" && network != "tcp6" {
-					return nil, errors.New("unsupported network " + network)
-				}
+    return &http.Client{
+        Transport: &http.Transport{
+            DialContext: func(context context.Context, network, address string) (net.Conn, error) {
+                if network != "tcp" && network != "tcp4" && network != "tcp6" {
+                    return nil, errors.New("unsupported network " + network)
+                }
 
-				left, right := net.Pipe()
+                left, right := net.Pipe()
 
-				in <- inbound.NewHTTP(address, source, right)
+                in <- inbound.NewHTTP(address, source, right)
 
-				return left, nil
-			},
-		},
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-		Timeout: defaultClientTimeout,
-	}
+                return left, nil
+            },
+        },
+        CheckRedirect: func(req *http.Request, via []*http.Request) error {
+            return http.ErrUseLastResponse
+        },
+        Timeout: defaultClientTimeout,
+    }
 }
