d0a73321f0f79e54fec4c53e2eb0fb36ac49fccc
Kr328 <kr328app@outlook.com>
2021-06-15 14:03:17 +0800

    http: use timeout from http.DefaultTransport

--- listener/http/client.go
+++ listener/http/client.go
@@ -11,13 +11,15 @@ import (
 	C "github.com/Dreamacro/clash/constant"
 )
 
-const (
-	defaultClientTimeout = time.Minute
-)
-
 func newClient(source net.Addr, in chan<- C.ConnContext) *http.Client {
 	return &http.Client{
 		Transport: &http.Transport{
+			// from http.DefaultTransport
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ResponseHeaderTimeout: 10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
 			DialContext: func(context context.Context, network, address string) (net.Conn, error) {
 				if network != "tcp" && network != "tcp4" && network != "tcp6" {
 					return nil, errors.New("unsupported network " + network)
@@ -33,6 +35,5 @@ func newClient(source net.Addr, in chan<- C.ConnContext) *http.Client {
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
-		Timeout: defaultClientTimeout,
 	}
 }
