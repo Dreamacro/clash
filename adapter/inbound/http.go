942ea229ceab52ece2313783ca36133bae5c260e
Kr328 <kr328app@outlook.com>
2021-06-15 13:42:46 +0800

    fix: fix http conn network type

--- adapter/inbound/http.go
+++ adapter/inbound/http.go
@@ -11,6 +11,7 @@ import (
 // NewHTTP receive normal http request and return HTTPContext
 func NewHTTP(target string, source net.Addr, conn net.Conn) *context.ConnContext {
 	metadata := parseSocksAddr(socks5.ParseAddr(target))
+	metadata.NetWork = C.TCP
 	metadata.Type = C.HTTP
 	if ip, port, err := parseAddr(source.String()); err == nil {
 		metadata.SrcIP = ip
